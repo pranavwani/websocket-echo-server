32316ddb77ac67d39a458ea5c35c343cf1a201b9
Pranav <pranav@jsbot.io>
2020-10-28 23:44:34 +0530

    node_modules removed

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const results = Object.create(null); // or just '{}', an empty object
 
 for (const name of Object.keys(nets)) {
     for (const net of nets[name]) {
+      console.log(net, nets[name]);
         // skip over non-ipv4 and internal (i.e. 127.0.0.1) addresses
         if (net.family === 'IPv4' && !net.internal) {
             if (!results[name]) {
