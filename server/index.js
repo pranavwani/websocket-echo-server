bb92f8eefd9cbd53269cc691b3324577be54c863
Pranav <pranav@jsbot.io>
2020-10-28 23:27:39 +0530

    node_modules removed

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 import WebSocket from "ws";
-import https from "https";
+import http from "http";
 import fs from "fs";
 import { dirname } from "path";
 import url, { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const server = https.createServer({
-  cert: fs.readFileSync(__dirname + "/certificates/cert.pem"),
-  key: fs.readFileSync(__dirname + "/certificates/key.pem"),
-});
+const server = http.createServer(
+  // {
+  // cert: fs.readFileSync(__dirname + "/certificates/cert.pem"),
+  // key: fs.readFileSync(__dirname + "/certificates/key.pem"),
+// }
+);
 
 const wss = new WebSocket.Server({ noServer: true });
 
@@ -21,7 +23,6 @@ wss.on("connection", function connection(ws) {
 });
 
 server.on("upgrade", (request, socket, head) => {
-
   console.log(request.connection.localAddress);
 
   if (request.headers.upgrade.toLowerCase() !== "websocket") {
@@ -29,7 +30,7 @@ server.on("upgrade", (request, socket, head) => {
     return;
   }
 
-  wss.handleUpgrade(request, socket, head, function done (ws) {
+  wss.handleUpgrade(request, socket, head, function done(ws) {
     console.log("upgrade");
     const pathname = url.parse(request.url).pathname;
 
@@ -47,6 +48,6 @@ wss.on("error", (error) => {
   console.log("Error: ", error);
 });
 
-server.listen(process.env.PORT || 5000)
+server.listen(process.env.PORT || 5000);
 
-console.log(process.env.PORT, server.address().address);
+console.log(process.env.PORT, server.address());
